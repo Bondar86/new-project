0908a8a79ef1282992481e077c523bc8850a6782
Ivan <xray86rus@gmail.com>
2023-05-05 10:14:16 +0500

    first commit

---
+++ new-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new-project
+//
+//  Created by Иван Бондаренко on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
